6c9ef8c5e48dd448bb9963e74c48d3c591340fc2
Ares1421 <arifinhussain001@gmail.com>
2023-02-18 16:25:12 +0600

    someChanges

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
+// document.getElementById('btn-submit').addEventListener('click', function () {
+//     const emailField = document.getElementById('user-email');
+//     const email = emailField.value;
+//     // get user password
+//     const passwordField = document.getElementById('user-password');
+//     const password = passwordField.value;
+//     // check email and password
+//     console.log(email, password);
+
+//     //DO NOT VERIFY email and password on this client side
+//     if (email === 'ares@gmail.com' && password === '123456') {
+//         window.location.href = 'bank.html';
+//     }
+//     else {
+//         alert('Invalid user');
+//     }
+// });
+
 document.getElementById('btn-submit').addEventListener('click', function () {
+    // get user email
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
     // get user password
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
-    // check email and password  
-    console.log(email, password);
-
+    // check email and password
     //DO NOT VERIFY email and password on this client side
-    if (email === 'ares@gmail.com' && password === '123456') {
+    if (email == 'ares@gmail.com' || password == '123456') {
         window.location.href = 'bank.html';
     }
     else {
